d768e8a1ff385409eb340dfdc357f4a9149f8c14
jayastronomic <juliansmith94@hotmail.com>
2023-10-19 16:52:42 -0500

    initial commit

---
+++ src/main/java/com/app/nailcare/NailcareApplication.java
@@ -0,0 +1,13 @@
+package com.app.nailcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/nailcare/NailcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.nailcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
