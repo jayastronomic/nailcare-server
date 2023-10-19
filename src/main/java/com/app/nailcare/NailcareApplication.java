f06da9e2dcae02754450193eca0583332b502efe
jayastronomic <juliansmith94@hotmail.com>
2023-10-19 17:20:08 -0500

    Update name of Application

--- src/main/java/com/app/nailcare/NailcareApplication.java
+++ src/main/java/com/app/nailcare/NailcareApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NailcareApplication {
+public class NailCareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NailcareApplication.class, args);
+		SpringApplication.run(NailCareApplication.class, args);
 	}
 
 }
