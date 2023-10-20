6278073664b25f95b6ab1130f35b01ac8e1a37d9
jayastronomic <juliansmith94@hotmail.com>
2023-10-20 10:24:31 -0500

    Implement Profile Entity

--- src/main/java/com/app/nailcare/models/Profile.java
+++ src/main/java/com/app/nailcare/models/Profile.java
@@ -1,4 +1,47 @@
 package com.app.nailcare.models;
 
-public class Profile {
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "profiles")
+public class Profile extends ApplicationEntity<Profile> {
+    @Column(nullable = false)
+    @NotBlank(message = "First name can not be blank")
+    private String firstName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Last name can not be blank")
+    private String lastName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "City can not be blank")
+    private String city;
+
+    @Column(nullable = false)
+    @NotBlank(message = "State can not be blank")
+    private String state;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Street address can not be blank")
+    private String streetAddress;
+
+    @Column(nullable = false)
+    @NotNull(message = "Birth date can not be blank")
+    private LocalDate birthdate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
+
