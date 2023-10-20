d0858c660eed3783713d0952f05408c80e3b37f9
jayastronomic <juliansmith94@hotmail.com>
2023-10-19 22:33:09 -0500

    Add Stripe-Java dependency to pom.xml

--- src/main/java/com/app/nailcare/controllers/StripePaymentController.java
+++ src/main/java/com/app/nailcare/controllers/StripePaymentController.java
@@ -1,4 +1,10 @@
 package com.app.nailcare.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StripePaymentController {
+    @Value("${stripe.apikey}")
+    String stripeKey;
 }
