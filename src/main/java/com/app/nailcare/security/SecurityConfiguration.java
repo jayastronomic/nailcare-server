a6266f7d0f2626f1510688f5d5a625005c373e49
jayastronomic <juliansmith94@hotmail.com>
2023-10-20 00:41:46 -0500

    Add Password Encoder Bean

--- src/main/java/com/app/nailcare/security/SecurityConfiguration.java
+++ src/main/java/com/app/nailcare/security/SecurityConfiguration.java
@@ -1,5 +1,15 @@
 package com.app.nailcare.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
