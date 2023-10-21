64531ed4a0a449cdfb491581dc1d359d7c21519e
jayastronomic <juliansmith94@hotmail.com>
2023-10-21 12:13:42 -0500

    Add instance variables and constructor to AuthService

--- src/main/java/com/app/nailcare/security/SecurityConfiguration.java
+++ src/main/java/com/app/nailcare/security/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.app.nailcare.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -33,4 +35,10 @@ public class SecurityConfiguration {
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
 }
--- src/main/java/com/app/nailcare/services/AuthService.java
+++ src/main/java/com/app/nailcare/services/AuthService.java
@@ -1,4 +1,28 @@
 package com.app.nailcare.services;
 
+import com.app.nailcare.repositories.UserRepository;
+import com.app.nailcare.security.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
 public class AuthService {
+    private final Logger logger = Logger.getLogger(AuthService.class.getName());
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder, JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+    }
 }
