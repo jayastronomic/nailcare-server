047ed5fc6e792591c05140e265304f45bc9c93e9
jayastronomic <juliansmith94@hotmail.com>
2023-10-21 10:03:48 -0500

    Add generateJwtToken method to JWTUtils

--- src/main/java/com/app/nailcare/security/JWTUtils.java
+++ src/main/java/com/app/nailcare/security/JWTUtils.java
@@ -1,4 +1,30 @@
 package com.app.nailcare.security;
 
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
 public class JWTUtils {
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
+
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpirationMs;
+
+
+    public String generateJwtToken(AuthUserDetails authUserDetails) {
+        return Jwts.builder()
+                .setSubject(authUserDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
 }
