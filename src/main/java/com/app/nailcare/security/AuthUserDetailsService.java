df9c8b3c473146e86322d00bb7e281fc1aab0a37
jayastronomic <juliansmith94@hotmail.com>
2023-10-20 19:30:15 -0500

    Implement AuthUserDetailsService

--- src/main/java/com/app/nailcare/security/AuthUserDetailsService.java
+++ src/main/java/com/app/nailcare/security/AuthUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.app.nailcare.security;
 
+import com.app.nailcare.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +9,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthUserDetailsService implements UserDetailsService {
+    public final UserRepository userRepository;
+
+    @Autowired
+    public AuthUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return new AuthUserDetails(userRepository.findByEmail(email).orElseThrow());
     }
 }
