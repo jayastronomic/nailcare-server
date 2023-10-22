0591a2dcc71f3378b44bb0a28161303ce5a0b2b3
jayastronomic <juliansmith94@hotmail.com>
2023-10-22 12:21:16 -0500

    Implement SubscriptionRepository interface

--- src/main/java/com/app/nailcare/repositories/SubscriptionRepository.java
+++ src/main/java/com/app/nailcare/repositories/SubscriptionRepository.java
@@ -1,4 +1,11 @@
 package com.app.nailcare.repositories;
 
-public class SubscriptionRepository {
+import com.app.nailcare.models.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
 }
