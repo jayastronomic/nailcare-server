07069033a8bc2a6c66df90541524459367706664
jayastronomic <juliansmith94@hotmail.com>
2023-10-22 12:40:13 -0500

    Implement CoverageRepository interface

--- src/main/java/com/app/nailcare/repositories/CoverageRepository.java
+++ src/main/java/com/app/nailcare/repositories/CoverageRepository.java
@@ -1,4 +1,10 @@
 package com.app.nailcare.repositories;
 
-public class CoverageRepository {
-}
+import com.app.nailcare.models.Coverage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CoverageRepository extends JpaRepository<Coverage, UUID> {}
