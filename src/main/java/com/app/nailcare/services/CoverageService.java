6a66e7e0bef91b4c39f35d5a3bc7daeae0223061
jayastronomic <juliansmith94@hotmail.com>
2023-10-24 11:58:34 -0500

    Add index method to CoverageService class

--- src/main/java/com/app/nailcare/services/CoverageService.java
+++ src/main/java/com/app/nailcare/services/CoverageService.java
@@ -1,4 +1,23 @@
 package com.app.nailcare.services;
 
+import com.app.nailcare.models.Coverage;
+import com.app.nailcare.repositories.CoverageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CoverageService {
+    private final CoverageRepository coverageRepository;
+
+    @Autowired
+    public CoverageService(CoverageRepository coverageRepository){
+        this.coverageRepository = coverageRepository;
+    }
+
+
+    public List<Coverage> index(){
+        return this.coverageRepository.findAll();
+    }
 }
