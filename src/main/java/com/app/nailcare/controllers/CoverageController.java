f905624797c8fe5e0aad109cbbe0846e8d36b08d
jayastronomic <juliansmith94@hotmail.com>
2023-10-24 12:05:14 -0500

    Add index method to CoverageController class

--- src/main/java/com/app/nailcare/controllers/CoverageController.java
+++ src/main/java/com/app/nailcare/controllers/CoverageController.java
@@ -1,4 +1,26 @@
 package com.app.nailcare.controllers;
 
+import com.app.nailcare.models.Coverage;
+import com.app.nailcare.responses.APIResponse;
+import com.app.nailcare.services.CoverageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/coverages")
 public class CoverageController {
+    private final CoverageService coverageService;
+
+    @Autowired
+    public CoverageController(CoverageService coverageService){
+        this.coverageService = coverageService;
+    }
+
+    public ResponseEntity<APIResponse<List<Coverage>>> index(){
+        return ResponseEntity.ok(new APIResponse<>(coverageService.index(), "success"));
+    }
 }
