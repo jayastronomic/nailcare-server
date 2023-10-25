408fa42ab73ebdcfea0315d280ef663dd5ceaa70
jayastronomic <juliansmith94@hotmail.com>
2023-10-25 10:51:06 -0500

    Add create method to ProfileService class

--- src/main/java/com/app/nailcare/services/ProfileService.java
+++ src/main/java/com/app/nailcare/services/ProfileService.java
@@ -1,11 +1,12 @@
 package com.app.nailcare.services;
 
+import com.app.nailcare.models.Profile;
 import com.app.nailcare.repositories.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProfileService {
+public class ProfileService extends ApplicationService {
     public final ProfileRepository profileRepository;
 
     @Autowired
@@ -13,5 +14,8 @@ public class ProfileService {
         this.profileRepository = profileRepository;
     }
 
-
+    public Profile create(Profile payload){
+        payload.setUser(currentUser());
+        return  profileRepository.save(payload);
+    }
 }
