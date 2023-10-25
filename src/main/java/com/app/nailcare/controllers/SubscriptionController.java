7f329cdf40fbceb650aa88ce80900bb67d49116c
jayastronomic <juliansmith94@hotmail.com>
2023-10-25 12:21:38 -0500

    Add create method to SubscriptionController class

--- src/main/java/com/app/nailcare/controllers/SubscriptionController.java
+++ src/main/java/com/app/nailcare/controllers/SubscriptionController.java
@@ -1,4 +1,31 @@
 package com.app.nailcare.controllers;
 
+import com.app.nailcare.models.Coverage;
+import com.app.nailcare.models.Subscription;
+import com.app.nailcare.requests.SubscriptionRequest;
+import com.app.nailcare.responses.APIResponse;
+import com.app.nailcare.services.SubscriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/subscriptions")
 public class SubscriptionController {
+    private final SubscriptionService subscriptionService;
+
+    @Autowired
+    public SubscriptionController(SubscriptionService subscriptionService){
+        this.subscriptionService = subscriptionService;
+    }
+
+    public ResponseEntity<APIResponse<Subscription> create(@RequestBody SubscriptionRequest payload){
+        return  ResponseEntity
+                .created(URI.create("/api/v1/subscriptions"))
+                .body(new APIResponse<>(subscriptionService.create(payload.getCoverageId()), "success"));
+    }
 }
--- src/main/java/com/app/nailcare/requests/SubscriptionRequest.java
+++ src/main/java/com/app/nailcare/requests/SubscriptionRequest.java
@@ -1,7 +1,10 @@
 package com.app.nailcare.requests;
 
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
 public class SubscriptionRequest {
     private UUID coverageId;
 }
