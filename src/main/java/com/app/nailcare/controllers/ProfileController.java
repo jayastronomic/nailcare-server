1619fada04dde7507814a5dae36c8409589d0102
jayastronomic <juliansmith94@hotmail.com>
2023-10-25 10:54:52 -0500

    Add create method to ProfileController class

--- src/main/java/com/app/nailcare/controllers/ProfileController.java
+++ src/main/java/com/app/nailcare/controllers/ProfileController.java
@@ -1,4 +1,30 @@
 package com.app.nailcare.controllers;
 
+import com.app.nailcare.models.Profile;
+import com.app.nailcare.responses.APIResponse;
+import com.app.nailcare.services.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/profiles")
 public class ProfileController {
+    private final ProfileService profileService;
+
+    @Autowired
+    public ProfileController(ProfileService profileService){
+        this.profileService = profileService;
+    }
+
+    public ResponseEntity<APIResponse<Profile>> create(@Valid @RequestBody Profile payload){
+        return ResponseEntity
+                .created(URI.create("/api/v1/profiles"))
+                .body(new APIResponse<>(profileService.create(payload), "success"));
+    }
 }
