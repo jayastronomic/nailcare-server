1bcf56cdec4e160cf2bc84a67ee8a6decc6cfb3e
jayastronomic <juliansmith94@hotmail.com>
2023-10-25 10:46:45 -0500

    Create ApplicationService class

--- src/main/java/com/app/nailcare/repositories/ProfileRepository.java
+++ src/main/java/com/app/nailcare/repositories/ProfileRepository.java
@@ -2,8 +2,10 @@ package com.app.nailcare.repositories;
 
 import com.app.nailcare.models.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProfileRepository extends JpaRepository<Profile, UUID> {
 }
--- src/main/java/com/app/nailcare/services/ProfileService.java
+++ src/main/java/com/app/nailcare/services/ProfileService.java
@@ -1,4 +1,17 @@
 package com.app.nailcare.services;
 
+import com.app.nailcare.repositories.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProfileService {
+    public final ProfileRepository profileRepository;
+
+    @Autowired
+    public ProfileService(ProfileRepository profileRepository){
+        this.profileRepository = profileRepository;
+    }
+
+
 }
