c378e83ba8d263f695b410e3dcbc569a28547808
jayastronomic <juliansmith94@hotmail.com>
2023-10-25 12:17:01 -0500

    Add create method to SubscriptionService

--- src/main/java/com/app/nailcare/requests/SubscriptionRequest.java
+++ src/main/java/com/app/nailcare/requests/SubscriptionRequest.java
@@ -1,4 +1,7 @@
 package com.app.nailcare.requests;
 
+import java.util.UUID;
+
 public class SubscriptionRequest {
+    private UUID coverageId;
 }
--- src/main/java/com/app/nailcare/services/SubscriptionService.java
+++ src/main/java/com/app/nailcare/services/SubscriptionService.java
@@ -1,19 +1,31 @@
 package com.app.nailcare.services;
 
+import com.app.nailcare.models.Coverage;
 import com.app.nailcare.models.Subscription;
+import com.app.nailcare.repositories.CoverageRepository;
 import com.app.nailcare.repositories.SubscriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
-public class SubscriptionService {
+public class SubscriptionService extends ApplicationService {
     private final SubscriptionRepository subscriptionRepository;
+    private final CoverageRepository coverageRepository;
 
     @Autowired
-    public SubscriptionService(SubscriptionRepository subscriptionRepository){
+    public SubscriptionService(SubscriptionRepository subscriptionRepository, CoverageRepository coverageRepository){
         this.subscriptionRepository = subscriptionRepository;
+        this.coverageRepository = coverageRepository;
     }
 
 
-
+    public Subscription create(UUID id){
+        Coverage coverage = coverageRepository.findById(id).orElseThrow();
+        Subscription newSubscription = new Subscription();
+        newSubscription.setCoverage(coverage);
+        newSubscription.setUser(currentUser());
+        return subscriptionRepository.save(newSubscription);
+    }
 }
