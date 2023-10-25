558654dadab1478d4e42c4753911b89c7e162f9a
jayastronomic <juliansmith94@hotmail.com>
2023-10-24 20:43:53 -0500

    Add show method to CoverageController class

--- src/main/java/com/app/nailcare/controllers/CoverageController.java
+++ src/main/java/com/app/nailcare/controllers/CoverageController.java
@@ -6,10 +6,12 @@ import com.app.nailcare.services.CoverageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/coverages")
@@ -24,5 +26,10 @@ public class CoverageController {
     @GetMapping
     public ResponseEntity<APIResponse<List<Coverage>>> index(){
         return ResponseEntity.ok(new APIResponse<>(coverageService.index(), "success"));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<APIResponse<Coverage>> show(@PathVariable(value = "id") UUID id){
+        return  ResponseEntity.ok(new APIResponse<>(coverageService.show(id), "success"));
     }
 }
