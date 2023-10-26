347043c9efc4c4aa65111b70194e3d604b9c5633
jayastronomic <juliansmith94@hotmail.com>
2023-10-25 19:00:55 -0500

    Add create method to ClaimController class

--- src/main/java/com/app/nailcare/controllers/ClaimController.java
+++ src/main/java/com/app/nailcare/controllers/ClaimController.java
@@ -1,4 +1,33 @@
 package com.app.nailcare.controllers;
 
+import com.app.nailcare.models.Claim;
+import com.app.nailcare.responses.APIResponse;
+import com.app.nailcare.services.ClaimService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/claims")
 public class ClaimController {
+    private final ClaimService claimService;
+
+    @Autowired
+    public ClaimController(ClaimService claimService){
+        this.claimService = claimService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<APIResponse<Claim>> create(@Valid @RequestBody Claim payload){
+        return  ResponseEntity
+                .created(URI.create("/api/v1/claims"))
+                .body(new APIResponse<>(claimService.create(payload), "success"));
+
+    }
 }
