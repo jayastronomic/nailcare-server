3d977898b2506557f3564fbd944ebe111ad1e3b1
jayastronomic <juliansmith94@hotmail.com>
2023-10-27 06:54:00 -0500

    Add Java Docs to Services

--- src/main/java/com/app/nailcare/services/CoverageService.java
+++ src/main/java/com/app/nailcare/services/CoverageService.java
@@ -8,23 +8,48 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * The CoverageService class provides services related to insurance coverage in the NailCare application.
+ */
 @Service
 public class CoverageService extends ApplicationService {
     private final CoverageRepository coverageRepository;
 
+    /**
+     * Create a CoverageService instance with the specified CoverageRepository.
+     *
+     * @param coverageRepository The repository for managing insurance coverage data.
+     */
     @Autowired
     public CoverageService(CoverageRepository coverageRepository){
         this.coverageRepository = coverageRepository;
     }
 
-
+    /**
+     * Retrieve a list of all available insurance coverage options.
+     *
+     * @return A list of all available insurance coverage options.
+     */
     public List<Coverage> index(){
         return coverageRepository.findAll();
     }
 
+    /**
+     * Retrieve details of a specific insurance coverage option based on its unique identifier (ID).
+     *
+     * @param id The unique identifier of the insurance coverage option to retrieve.
+     * @return The details of the specified insurance coverage option.
+     */
     public Coverage show(UUID id){
         return coverageRepository.findById(id).orElseThrow();
     }
 
-    public Coverage getUserCoverage(){ return currentUser().getSubscription().getCoverage() ;}
+    /**
+     * Retrieve the insurance coverage associated with the currently logged-in user's subscription.
+     *
+     * @return The insurance coverage associated with the user's subscription.
+     */
+    public Coverage getUserCoverage(){
+        return currentUser().getSubscription().getCoverage();
+    }
 }
--- src/main/java/com/app/nailcare/services/ProfileService.java
+++ src/main/java/com/app/nailcare/services/ProfileService.java
@@ -5,17 +5,31 @@ import com.app.nailcare.repositories.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * The ProfileService class provides services related to user profiles in the NailCare application.
+ */
 @Service
 public class ProfileService extends ApplicationService {
     public final ProfileRepository profileRepository;
 
+    /**
+     * Create a ProfileService instance with the specified ProfileRepository.
+     *
+     * @param profileRepository The repository for managing user profiles.
+     */
     @Autowired
     public ProfileService(ProfileRepository profileRepository){
         this.profileRepository = profileRepository;
     }
 
+    /**
+     * Create a user profile associated with the currently logged-in user.
+     *
+     * @param payload The user profile data to be created.
+     * @return The created user profile.
+     */
     public Profile create(Profile payload){
         payload.setUser(currentUser());
-        return  profileRepository.save(payload);
+        return profileRepository.save(payload);
     }
 }
